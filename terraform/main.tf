ce8ec7be734b3935b46ae9464d7eb00523a1080d
Eugene2016 <evgeny@infinit-group.com>
2023-06-06 18:00:08 +0300

    1 networking

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,8 @@
 
-# module "ssh_keys" {
-#   source   = "./modules/ssh_keys"
-#   key_pair = var.key_pair_names
-# }
-
+module "ssh_keys" {
+  source   = "./modules/ssh_keys"
+  key_pair = var.key_pair_names
+}
 
 module "networking" {
   source    = "./modules/network"
@@ -12,30 +11,33 @@ module "networking" {
   #jenkins_target_group_arn = module.jenkins.jenkins-server-target-group-arn
   #elastic_target_group_arn = module.elastic.elastic_target_group_arn
  } 
-/*
-module "jenkins" {
-  source    = "./modules/jenkins"
-  vpc_id = module.networking.vpcid
-  # vpn_sg = module.vpn.vpn_sg
-  # server_public_key = module.ssh_keys.servers_key[1]
-  # servers_private_key = module.ssh_keys.servers_private_key[1]
-  # subnet_id = module.networking.private-subnet-id
-  # availability_zone = var.availability_zone
-  # ami = var.ami
-  region = var.region
-  # consul_iam_instance_profile = module.consul.aws_iam_instance_profile
-  # consul_security_group = module.consul.consul_security_group_id
-  # consul_join_policy_arn= module.consul.consul_join_policy_arn
-} 
-*/
-module "k8s" {
-  source    = "./modules/k8s"
-  vpc_id = module.networking.vpcid
-  region = var.region
-  subnet_ids = module.networking.private_subnet_id
-  #role_arn  = module.jenkins.jenkins_role_arn
-  #role_name = module.jenkins.jenkins_role_name
-}
+
+# module "jenkins" {
+#   source    = "./modules/jenkins"
+#   vpc_id = module.networking.vpcid
+#   # vpn_sg = module.vpn.vpn_sg
+#   # server_public_key = module.ssh_keys.servers_key[1]
+#   # servers_private_key = module.ssh_keys.servers_private_key[1]
+#   # subnet_id = module.networking.private-subnet-id
+#   # availability_zone = var.availability_zone
+#   # ami = var.ami
+#   region = var.region
+#   # consul_iam_instance_profile = module.consul.aws_iam_instance_profile
+#   # consul_security_group = module.consul.consul_security_group_id
+#   # consul_join_policy_arn= module.consul.consul_join_policy_arn
+# } 
+
+
+
+# module "k8s" {
+#   source    = "./modules/k8s"
+#   vpc_id = module.networking.vpcid
+#   region = var.region
+#   subnet_ids = module.networking.private_subnet_id
+#   #role_arn  = module.jenkins.jenkins_role_arn
+#   #role_name = module.jenkins.jenkins_role_name
+# }
+
 # module "monitoring" {
 #   source    = "./modules/monitor"
 #   # region = var.region
--- terraform/output.tf
+++ terraform/output.tf
@@ -6,20 +6,20 @@
 #   value = module.ssh_keys.servers_key
 # }
 
-output "k8s_cluster_id" {
-  description = "EKS cluster ID."
-  value       = module.k8s.cluster_id
-} 
+# output "k8s_cluster_id" {
+#   description = "EKS cluster ID."
+#   value       = module.k8s.cluster_id
+# } 
 
-output "k8s_cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.k8s.cluster_endpoint
-} 
+# output "k8s_cluster_endpoint" {
+#   description = "Endpoint for EKS control plane."
+#   value       = module.k8s.cluster_endpoint
+# } 
 
-output "k8s_oidc_provider_arn" {
-  value = module.k8s.oidc_provider_arn
-} 
+# output "k8s_oidc_provider_arn" {
+#   value = module.k8s.oidc_provider_arn
+# } 
 
-output "k8s_cluster_name" {
-  value = module.k8s.cluster_name
-} 
+# output "k8s_cluster_name" {
+#   value = module.k8s.cluster_name
+# } 
